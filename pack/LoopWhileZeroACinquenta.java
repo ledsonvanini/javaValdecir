27d82f685bc9bdec6d54b2a617c985671ccb8008
Ledson Vanini <ledsonvanini@gmail.com>
2023-11-23 22:25:57 +0000

    LoopWhileZeroACinquenta

---
+++ pack/LoopWhileZeroACinquenta.java
@@ -0,0 +1,15 @@
+/**
+ * Escreva um programa em Java com uma variável 
+ * do tipo int que imprima na tela os números de 0 a 50.
+ * Compile e poste o resultado aqui.
+ */
+
+public class LoopWhileZeroACinquenta {
+    public static void main(String[] args) {
+        int aux = 0;
+        while(aux <= 50) {
+            System.out.println("Nº: " + aux);
+            aux++;
+        }
+    }
+}
